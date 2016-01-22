9e8134f5c33b937b88acffc484c6d3a463c851ee
cwillaim <cwwomack@mit.edu>
2016-01-22 00:25:34 -0500

    Use default language, send calls to better route

--- been-twil/routes/index.js
+++ been-twil/routes/index.js
@@ -9,10 +9,10 @@ var fizzbuzz = require('../fizzbuzz');
 //   res.render('index', { title: 'Express' });
 // });
 
-router.get('/', function(req, res) {
+router.get('/incoming', function(req, res) {
 	var twiml = twilio.twiml.build(function(response) {
 		response.gather(function(rez) {
-			rez.say("Please enter a number greater than zero followed by the star key.", { voice: 'man', language: 'en-gb' });
+			rez.say("Please enter a number greater than zero followed by the star key.");
 		}, {action: '/fizzbuzz', method: 'POST', timeout: 10, finishOnKey: '*'})
 	});
 	res.send(twiml, {'Content-Type':'text/xml'}, 200);
@@ -21,8 +21,8 @@ router.get('/', function(req, res) {
 router.post('/fizzbuzz', function(req, res) {
 	var value = req.body.Digits;
 	var twiml = twilio.twiml.build(function(response) {
-		response.say(fizzbuzz.generateResponse(value), { voice: 'man', language: 'en-gb' });
-		response.say("Goodbye", { voice: 'woman', language: 'en-gb' });
+		response.say(fizzbuzz.generateResponse(value));
+		response.say("Goodbye");
 		response.hangup();
 	});
 	res.send(twiml, {'Content-Type': 'text/xml'}, 200);
