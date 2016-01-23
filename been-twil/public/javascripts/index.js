3b38450e6d5a414ef32e9823add72218f70b3a66
cwillaim <cwwomack@mit.edu>
2016-01-22 20:29:08 -0500

    Implement call delay functionality

--- been-twil/public/javascripts/index.js
+++ been-twil/public/javascripts/index.js
@@ -1,10 +1,14 @@
 (function() {
 	$('#number-form').submit(function(evt) {
 		evt.preventDefault();
-		var number = $("#mobile-number").intlTelInput("getNumber");
+		var phoneNumber = $("#mobile-number").intlTelInput("getNumber"),
+			formData = helpers.getFormData(this),
+			delay;
+		delay = (formData.hours * 3600) + (formData.minutes * 60) + formData.seconds;
 		$.post(
 			"/outgoing",
-			{ To: number }
+			{ to: phoneNumber,
+			  delay: delay }
 		).done(function(response) {
 
 		}).fail(function(responseObject) {
--- been-twil/routes/index.js
+++ been-twil/routes/index.js
@@ -16,10 +16,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'PhoneBuzz' });
 });
 
-router.get('/incoming', function(req, res) {
+router.post('/handleCall', function(req, res) {
 	var twiml = twilio.twiml.build(function(response) {
 		response.gather(function(rez) {
-			rez.say("Please enter a number greater than zero followed by the star key.");
+			rez.say("Please enter a number greater than zero and the star key.", { voice: 'woman', language: 'en-gb' });
 		}, {action: '/fizzbuzz', method: 'POST', timeout: 5, finishOnKey: '*'})
 	});
 	res.send(twiml, {'Content-Type':'text/xml'}, 200);
@@ -28,27 +28,30 @@ router.get('/incoming', function(req, res) {
 router.post('/fizzbuzz', function(req, res) {
 	var value = req.body.Digits;
 	var twiml = twilio.twiml.build(function(response) {
-		response.say(fizzbuzz.generateResponse(value));
-		response.say("Thank you. Goodbye.");
+		response.say(fizzbuzz.generateResponse(value), { voice: 'woman', language: 'en-gb' });
+		response.say("Thank you, goodbye.", { voice: 'woman', language: 'en-gb' });
 		response.hangup();
 	});
 	res.send(twiml, {'Content-Type': 'text/xml'}, 200);
 });
 
 router.post('/outgoing', function(req, res) {
-	if (!req.body.To) req.body.To = '+15165672905';
-	client.makeCall({
+	if (!req.body.to) req.body.to = '+15165672905';
+	setTimeout(function(){
+		client.makeCall({
 
-	    to: req.body.To,
+	    to: req.body.to,
 	    from: '+15162523246',
-	    url: 'http://phonebuzzz.herokuapp.com/incoming',
-	    method: 'GET'
+	    url: 'http://phonebuzzz.herokuapp.com/handleCall',
+	    method: 'POST'
 
-	}, function(err, responseData) {
-	    if (!err) {
-	        console.log(responseData.from);
-	    }
-	});
+		}, function(err, responseData) {
+		    if (!err) {
+		        console.log(responseData.from);
+		    }
+		});
+	}, req.body.delay*1000)
+
 });
 
 module.exports = router;
